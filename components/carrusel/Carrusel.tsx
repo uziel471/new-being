735b17d5bf12b12e18b035e5344592de8bdca33a
uziel471 <uziel.estrada@g-global.com>
2022-11-22 22:57:25 -0800

    fix: eslint

--- components/carrusel/Carrusel.tsx
+++ components/carrusel/Carrusel.tsx
@@ -1,7 +1,7 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-interface Props {
+type Props = {
     imagesNames: string[];
 }
 
