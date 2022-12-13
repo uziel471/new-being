227a5e4e3a67dd375b1586d9fc6260d83d54d9d0
David070180 <120416655+David070180@users.noreply.github.com>
2022-12-13 13:42:52 -0800

    Update ObjectivesSection.tsx

--- pages/sobre-nosotros/components/ObjectivesSection.tsx
+++ pages/sobre-nosotros/components/ObjectivesSection.tsx
@@ -8,19 +8,15 @@ export default function ObjectivesSection() {
             <div className='p-10'>
                 <h1 className="text-4xl font-bold">Objetivos</h1>
                 <p className="lg:text-lg mt-4 md:text-md text-sm">
-                    Brindamos a la persona tratando su patología un seguimiento
-                    óptimo en su proceso de recuperación, otorgando herramientas
-                    para cumplir
-                    con los objetivos de nuestros programas, manteniendo una{' '}
-                    <b>conexión</b>
-                    <b>real con sí mismo y su familia.</b>
-                    Un proceso que tiene como objetivo el{' '}
-                    <b>estímulo y reinserción</b> óptima
-                    al entorno en general, el cual es complementado con valores
-                    como
-                    respeto, dignidad y confianza, donde acompañamos a la
-                    persona en
-                    el desarrollo de habilidades.
+                    Brindar un servicio profesional sustentado en los avances
+                    científicos en materia de salud mental, especialmente en el
+                    tema de los trastornos en sus diversas manifestaciones.
+                    Ello aunado a una experienciar de 30 anos en los cuales
+                    convergen la medicina, la psiquiatría, la psicología,
+                    las ciencias de la comunicación, las neurociencias,
+                    el deporte, entre otros. Se pretende lograr la integración
+                    psicoespiritual del paciente en un proceso
+                    de desarrollo humano integral.
                 </p>
             </div>
             <div className=''>
