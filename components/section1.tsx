11e5223f8c3225619ec4d03058ed048ac2676a6d
David070180 <120416655+David070180@users.noreply.github.com>
2022-12-13 15:40:27 -0800

    Update section1.tsx

--- components/section1.tsx
+++ components/section1.tsx
@@ -3,17 +3,18 @@ export default function Section1() {
     <div className="grid lg:grid-cols-2 md:grid-cols-2 lg:gap-4 place-items-center font-sans lg:p-20 md:p-6 p-10">
       <div className="grid grid-rows-2">
         <div className="pb-8">
-          <p className="lg:text-6xl md:text-4xl text-2xl">Contamos con 30 años de experiencia</p>
+          <p className="lg:text-5xl md:text-3xl text-2xl">Contamos con 30 años de experiencia</p>
         </div>
         <div>
           <p className="lg:text-2xl text-sm md:text-md">
             Clínica privada para el tratamiento de trastornos<br/>
             de personalidad, salud mental y codependencia en Tijuana, México.<br/>
-            Al servicio y cuidado de personas que padecen trastornos de personalidad,<br/>
+            Al servicio y cuidado de personas que padecen trastornos de personalidad,
             problemas de salud mental y codependencia.<br/>
             Formamos una alianza poderosa entre la ciencia aplicada<br/>
             a la salud mental, la ética profesional y el compromiso<br/>
-            responsable de los familiares de nuestros pacientes.
+            responsable de los familiares de nuestros pacientes.<br/>
+            <br/>
           </p>
         </div>
       </div>
