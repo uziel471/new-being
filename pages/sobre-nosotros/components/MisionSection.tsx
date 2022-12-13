b0f7e3a0ab212092bbd9c149c7701162885d5542
David070180 <120416655+David070180@users.noreply.github.com>
2022-12-13 14:33:22 -0800

    Update MisionSection.tsx

--- pages/sobre-nosotros/components/MisionSection.tsx
+++ pages/sobre-nosotros/components/MisionSection.tsx
@@ -20,24 +20,18 @@ export default function MisionSection() {
                         </div>
                     </div>
                 <div className='p-10 lg:p-20'>
-                    <h1 className="text-4xl text-white font-bold">Mision</h1>
+                    <h1 className="text-4xl text-white font-bold">Misión</h1>
                     <p className="lg:text-lg md:text-md text-sm mt-4 text-white">
-                        Brindamos a la persona tratando su patología un seguimiento
-                        óptimo
-                        en su proceso de recuperación, <b>
-                            otorgando herramientas
-                        </b>{' '}
-                        para cumplir
-                        con los objetivos de nuestros programas, manteniendo una{' '}
-                        <b>conexión</b>
-                        <b>real con sí mismo y su familia.</b>
-                        Un proceso que tiene como objetivo el{' '}
-                        <b>estímulo y reinserción</b> óptima
-                        al entorno en general, el cual es complementado con valores
-                        como
-                        respeto, dignidad y confianza, donde acompañamos a la
-                        persona en
-                        el desarrollo de habilidades.
+                        En Clínica NUevo Ser creemos en la capacidad del ser humano
+                        para <b>trasnformarse a si mosmo y elevar su potencial al máximo</b>,
+                        sabemos que nuestros pacientes y sus familias buscan tranquilidad
+                        y entendimiento, así como una solución efectiva para liberarse y
+                        Reintegrarse a la vida.
+                        La buena voluntad, ética y calidad moral de nuestro <b>equipo
+                        multidisciplinario de profesionales</b>{' '} asegura un trato digno
+                        y humano que le permite a nuestros pacientes y a sus
+                        familiares sentirse atendidos y entendidos durante
+                        todo su proceso de recuperación.
                     </p>
                 </div>
             </div>
