5d3201b955366cfea9ec460263a9642d7d919a14
David070180 <120416655+David070180@users.noreply.github.com>
2022-12-13 13:39:20 -0800

    Update PlacesSection.tsx

--- pages/sobre-nosotros/components/PlacesSection.tsx
+++ pages/sobre-nosotros/components/PlacesSection.tsx
@@ -14,13 +14,13 @@ export default function PlacesSection() {
                 />
             </div>
             <div className="w-screen h-screen  lg:pl-36 flex flex-col justify-center">
-                <h3 className="md:text-5xl sm:text-3xl text-3xl font-bold">
-                    3 clínicas de atención
+                <h3 className="md:text-5xl sm:text-3xl text-shadow-3xl font-bold">
+                    3 Clínicas de atención
                 </h3>
 
                 <div className="ml-[10px] mt-[30px]">
                     <div>
-                        <button className="outline outline-offset-8 rounded text-2xl border-cyan-300 text-cyan-300">
+                        <button className="outline outline-offset-8 rounded text-shadow-2xl border-cyan-300 text-cyan-300">
                             <p className="text-white">CONOCE NUESTRAS INSTALACIONES</p>
                         </button>
                     </div>
