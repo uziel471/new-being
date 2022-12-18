2b374327ee8690c1b107f65c28a7ddc576a1c788
uziel471 <uziel.estrada@g-global.com>
2022-12-17 16:52:21 -0800

    fix boton conoce nuestras instalaciones

--- pages/sobre-nosotros/components/PlacesSection.tsx
+++ pages/sobre-nosotros/components/PlacesSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import image from '../../../public/assets/instalaciones.jpg';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function PlacesSection() {
     return (
@@ -14,15 +15,17 @@ export default function PlacesSection() {
                 />
             </div>
             <div className="w-screen h-screen  lg:pl-36 md:pl-[60px] flex flex-col justify-center">
-                <h3 className="md:text-5xl sm:text-3xl text-shadow-3xl font-bold">
+                <h3 className="md:text-5xl sm:text-3xl text-shadow-3xl font-bold ml-[40px]">
                     3 Clínicas de atención
                 </h3>
 
-                <div className="ml-[10px] mt-[30px]">
+                <div className="ml-[40px] mt-[30px] z-0">
                     <div>
-                        <button className="outline outline-offset-8 rounded text-shadow-2xl border-cyan-300 text-cyan-300">
-                            <p className="text-white">CONOCE NUESTRAS INSTALACIONES</p>
-                        </button>
+                        <Link href="/instalaciones">
+                            <button className="outline outline-offset-8 rounded text-shadow-2xl border-cyan-300 text-cyan-300">
+                                <p className="text-white">CONOCE NUESTRAS INSTALACIONES</p>
+                            </button>
+                        </Link>
                     </div>
                 </div>
             </div>
